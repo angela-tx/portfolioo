de2d46027805bc7d2972a7a9beabf3d2ec1eb192
Lucas <lucasgingera@outlook.com>
2026-03-20 01:07:03 -0700

    style: experience and spacing

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -80,7 +80,7 @@ export const Layout = () => {
       </div>
     </header>
 
-    <main className="mx-auto flex w-[min(800px,calc(100%-160px))] flex-col gap-[88px] pt-36 pb-24">
+    <main className="mx-auto flex w-[min(800px,calc(100%-160px))] flex-col gap-[88px] pt-36">
       <Outlet />
     </main>
     <Footer />
